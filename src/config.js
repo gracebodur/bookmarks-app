3ec4f155694824b582f6437ac16bd243cea8a6b6
Grace <mgracebodur@gmail.com>
2019-11-28 23:17:05 -0500

    Transfered supersecretkey

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$w8uTcVCIeb8xJIrcP0XuWOTNMaBspXScWml3KCOU6jB9cjahUtT.a',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
